ce6ba9a782c463cb90e2323d87f113d8a9efe140
zyorkie <142958671+zyorkie@users.noreply.github.com>
2023-08-23 12:55:47 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ my_fruit_list= my_fruit_list.set_index('Fruit') # to select name of fruit
 fruits_selected= streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries']) # user defined select bar
 # [Avocado, strawberres]filter, pre populated, already picked by customer
 
-fruits_to_show= my_fruits_list.loc[fruits_selected]
+fruits_to_show= my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
