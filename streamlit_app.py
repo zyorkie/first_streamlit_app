0b6efa4144541cbd0b9a3fd879be576e943bb73d
zyorkie <142958671+zyorkie@users.noreply.github.com>
2023-08-23 12:51:04 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com
 my_fruit_list= my_fruit_list.set_index('Fruit') # to select name of fruit
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries']) # user defined select bar
+fruits_selected= streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries']) # user defined select bar
 # [Avocado, strawberres]filter, pre populated, already picked by customer
 
 fruits_to_show= my_fruits_list.loc[fruits_selected]
