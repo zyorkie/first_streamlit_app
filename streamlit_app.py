b83601afa3b30be5af57a3c5c87732c2fa2c208b
zyorkie <142958671+zyorkie@users.noreply.github.com>
2023-08-24 14:08:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,21 +23,21 @@ fruits_selected= streamlit.multiselect("Pick some fruits:", list(my_fruit_list.i
 
 fruits_to_show= my_fruit_list.loc[fruits_selected]
 
-# Display the table on the page.
+#create repetable
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_respone= request.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
+#new section
 streamlit.header("Fruityvice Fruit Advice!")
 try: 
   fruit_choice = streamlit.text_input('What fruit would you like information about?','')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    back_from_function= get_fruityvice_data(fruit_choice)
     streamlit.dataframe(fruityvice_normalized)
-
-except URLError as e:
-  streamlit.error()
-
-
 
 #import snowflake.connector 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
